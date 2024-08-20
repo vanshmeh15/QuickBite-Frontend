9ea65656d99aad20c6369480f020b1f7c84b9854
vanshmeh15 <163817816+vanshmeh15@users.noreply.github.com>
2024-08-20 23:44:02 +0530

    Update StoreContext.jsx

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 export default function StoreContextProvider(props) {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:3000";
+  const url = "https://quickbite-backend-kjhn.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
